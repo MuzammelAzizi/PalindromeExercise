c7afd7e39cc57369864eef3eac0da51a6520cf5a
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-03-13 07:01:05 -0700

    status

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        //In your WordSmith class, you will create a method (IsAPalindrome)
+        //that will take a parameter of type string and return a value of type bool.
+        //If the given input is a palindrome – return true, else return false.
+        //You will finish this method after you write your tests.
+        public bool IsAPalindrome(string word) 
+        {
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            if (reversed == word) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("level", true)]
+        [InlineData("saga", false)]
+        [InlineData("modern", false)]
+        [InlineData("test", false)]
+        
+        public void PalindromeTest(string word, bool expected)
         {
+            //Arrange
+            // note pre code by instance of a class by method lives 
 
+            var test = new WordSmith();
+
+            //Act calling the method
+            var actual = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual); 
         }
     }
-}
+} 
